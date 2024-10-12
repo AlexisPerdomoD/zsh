1e03f9545bef159c5583d7b6e6147f1329b594ca
alexisperdomod <dropdibemol@gmail.com>
2024-10-12 15:17:44 -0500

    cshark termianl integration

--- exports.zsh
+++ exports.zsh
@@ -9,6 +9,14 @@ export PATH=$HOME/.local/kitty.app/bin:$PATH
 export LUA_PATH=$HOME/.config/wezterm/wezterm.lua:$LUA_PATH
 # neovim
 export PATH=$HOME/.local/share/nvim/mason/bin:$PATH
+# dotnet
+export PATH="$PATH:/usr/share/dotnet"
+# Required setup for dotnet
+# dotnet --list-runtimes
+# Microsoft.AspNetCore.App 8.0.8 [/usr/share/dotnet/shared/Microsoft.AspNetCore.App]
+# Microsoft.NETCore.App 6.0.33 [/usr/share/dotnet/shared/Microsoft.NETCore.App]
+# Microsoft.NETCore.App 8.0.8 [/usr/share/dotnet/shared/Microsoft.NETCore.App]
+
 # brew
 # eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
 # fzf integration 
